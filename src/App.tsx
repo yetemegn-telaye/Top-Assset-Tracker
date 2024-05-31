31f8b019b3e6961ba1d69f5fa009637bbfad326b
yetemegn-telaye <lemma19yet@gmail.com>
2024-05-31 17:49:57 +0300

    setup installations

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+    return (
+       <div>
+              <h1 className="text-primary">Hello, World!</h1>
+       </div>
+    );
+};
+
+export default App;
