f9653cb7aaf3be8faa255668178fc0d5491dd115
yetemegn-telaye <lemma19yet@gmail.com>
2024-05-31 18:26:30 +0300

    Setup theme with tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const App: React.FC = () => {
     return (
        <div>
-              <h1 className="text-primary">Hello, World!</h1>
+            <h1 className="text-primary-lighter">Hello, World!</h1>
+            <h1 className="text-secondary-lighter">Hello, World!</h1>
+            <h1 className="text-error-lighter">Hello, World!</h1>
+            <h1 className="text-accent-lighter">Hello, World!</h1>
        </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {
       colors: {
         primary: {
           DEFAULT: '#146A98',
-          light: '#3DB8E6',
+          light: 'rgba(20,106,152,0.6)',
+          lighter: 'rgba(20,106,152,0.2)',
         },
         secondary: {
           DEFAULT: '#008000',
-          light: 'rgba(0, 128, 0, 0.2)',
+          light: 'rgba(0,128,0,0.6)',
+          lighter: 'rgba(0,128,0,0.2)'
         },
         error: {
           DEFAULT: '#E10505',
-          light: 'rgba(225, 5, 5, 0.2)',
+          light: 'rgba(255,5,5,0.6)',
+          lighter: ' rgba(255,5,5,0.20)',
         },
         info: {
+          DEFAULT: '#3DB8E6',
+        },
+        accent: {
           DEFAULT: '#938E8E',
-          light: '#E3E3E1',
+          light: 'rgba(147,142,142,0.2)',
+          lighter: 'rgba(147,142,142,0.20)'
         },
         background: {
-          DEFAULT: '#FFFFFF',
-          paper: '#F7F9F5',
+          DEFAULT: '#F7F9F5',
+          paper: '#FFFFFF',
         },
       },
       fontFamily: {
-        sans: ['JetBrains Mono', 'monospace'],
+        jetbrainsmono: ['JetBrains Mono', 'monospace'],
       },
     },
   },
