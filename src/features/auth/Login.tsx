162a3608260294088a3612f01ff0b5ae4cc4236e
yetemegn-telaye <lemma19yet@gmail.com>
2024-05-31 21:47:41 +0300

    Create Login page layout

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,8 +1,40 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 const Login = () => {
     return (
+<div className="min-h-screen flex items-center justify-center">
+  <div className="bg-background-paper border border-gray-300 rounded-xl shadow-md w-50 m-auto flex flex-col max-h-screen items-center justify-center gap-8 p-8">
+    <div className='flex flex-col items-center justify-center gap-8'>
+      <FontAwesomeIcon icon={faUserCircle} size="5x" className="text-primary" />
+      <p className='text-xs text-accent'>Welcome to Top's Tracking System.</p>
+    </div>
+    <form className="flex flex-col space-y-4">
         <div>
-        <h1>Login</h1>
+        <input
+        type="text"
+        placeholder="Username"
+        className="p-2 border border-bottom border-gray-300"
+      />
         </div>
+        <div>
+      <input
+        type="password"
+        placeholder="Password"
+        className="p-2 border border-gray-300"
+      />
+      </div>
+      <button
+        type="submit"
+        className="p-2 bg-primary text-white"
+      >
+        Login
+      </button>
+      <button className='text-xs text-secondary-light font-light hover:text-secondary'>Create new account?</button>
+        
+    </form>
+  </div>
+</div>
+
     );
     }
 export default Login;
