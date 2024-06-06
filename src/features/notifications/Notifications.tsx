22ac94b7b38b2535e05ee5acacb57fc8dec0961d
yetemegn-telaye <lemma19yet@gmail.com>
2024-06-06 19:46:12 +0300

    Styled notifications page

--- src/features/notifications/Notifications.tsx
+++ src/features/notifications/Notifications.tsx
@@ -1,12 +1,45 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Layout from "../../components/layout/Layout";
+import { faBell, faShippingFast } from "@fortawesome/free-solid-svg-icons";
+import NotificationCard from "./NotificationCard";
 
 const Notifications = () => {
+    const notifications = [
+        {
+          message: 'Your item has been shipped'
+        },
+        {
+            message: 'Your item is waiting for approval'
+        },
+        {
+          message: 'Your item has been received'
+        },
+        {
+          message: 'Your item has been delayed'
+        },
+     
+      ];
+      
     return (
-        <div>
+       
             <Layout>
-                <h1>Notifications</h1>
+                <div className="flex flex-col bg-background-paper rounded-xl shadow-md p-8 pb-2 w-full h-screen overflow-y-auto">
+                    <div className="flex items-center gap-4 ml-4 mb-6 pb-6 mt-4">
+                    <FontAwesomeIcon icon={faBell} className="text-primary" size="xl" />
+                    <h1 className="text-2xl text-primary">
+                        Notifications
+                    </h1>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                    {notifications.map((notification, index) => (
+                        <NotificationCard key={index} notification={notification} />
+                      ))
+                    }
+                    </div>
+                     
+                </div>
             </Layout>  
-        </div>
+        
     );
     }
 export default Notifications;
