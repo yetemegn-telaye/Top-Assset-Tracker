e3e3dcc68906890a9d8186504e3caabcc01438b1
yetemegn-telaye <lemma19yet@gmail.com>
2024-06-07 14:50:04 +0300

    Fix styles

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,9 +9,9 @@ type LayoutProps = {
 
 const Layout: FC<LayoutProps> = ({ children }) => {
     return (
-        <div className="flex gap-3 h-screen pb-4">
+        <div className="flex gap-1 h-screen pb-3">
         <Sidebar />
-        <div className="flex flex-col gap-3 w-full m-3 mb-3">
+        <div className="flex flex-col gap-4 w-full m-3 mb-3">
         <SearchBar />
         {children}
         </div>
